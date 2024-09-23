7de9170abb2f2880c15937a1789beb2ce910027e
SergWebAcceptance <serg.web@acceptance.ltd>
2024-09-23 23:49:10 +0300

    added contact meta

--- src/app/contact-us/page.jsx
+++ src/app/contact-us/page.jsx
@@ -4,6 +4,18 @@ import ContactHero from "./_components/ContactHero";
 import ContactRequest from "./_components/ContactRequest";
 import ContactDetails from "./_components/ContactDetails";
 
+export const metadata = {
+    title: "Contact Quorixia: Get in Touch for Career Support",
+    description:
+      "Have questions or need career guidance? Contact Quorixia today to explore personalised HR and employment services tailored to your needs.",
+    openGraph: {
+      title: "Contact Quorixia: Get in Touch for Career Support",
+      description:
+        "Have questions or need career guidance? Contact Quorixia today to explore personalised HR and employment services tailored to your needs.",
+      images: "https://quorixia.com/images/meta.png",
+    },
+  };
+
 const ContactUs = () => {
   return (
     <>
