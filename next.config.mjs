678d531d996dfa9e193520b54b50ce7dc75d18ff
SergWebAcceptance <serg.web@acceptance.ltd>
2024-09-25 15:53:20 +0300

    fixed config

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-      domains: ['localhost', 'quorixia-cms.onrender.com'],
+      domains: ['localhost', 'quorixia-cms.onrender.com', 'cms.quorixia.com'],
     },
   };
   
