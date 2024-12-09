f66a711a325e6367126a3bbb424a201b784115d6
SergWebAcceptance <serg.web@acceptance.ltd>
2024-12-09 14:17:46 +0200

    terms

--- src/app/[locale]/(policies)/terms-of-use/page.jsx
+++ src/app/[locale]/(policies)/terms-of-use/page.jsx
@@ -2,7 +2,7 @@ import "@/styles/policy.scss";
 import { getPageBySlug, getPageSlugs } from "@/utils/pages";
 import React from "react";
 import ReactMarkdown from "react-markdown";
-import { setRequestLocale } from 'next-intl/server';
+import { setRequestLocale } from "next-intl/server";
 
 export async function generateStaticParams({ params: { locale } }) {
   setRequestLocale(locale);
@@ -10,17 +10,41 @@ export async function generateStaticParams({ params: { locale } }) {
   return slugs.map((slug) => ({ slug }));
 }
 
-export async function generateMetadata({ params: { locale } }) {
+const adjustedSlug = (locale, slug) => {
+  console.log("locale", locale);
+  console.log("slug", slug);
+
+  if (locale === "de") {
+    return slug.endsWith("-de") ? slug : `${slug}-de`;
+  } else if (locale === "it") {
+    return slug.endsWith("-it") ? slug : `${slug}-it`;
+  } else {
+    if (slug.endsWith("-de")) {
+      return slug.slice(0, -3); // Remove '-de'
+    } else if (slug.endsWith("-it")) {
+      return slug.slice(0, -3); // Remove '-it'
+    }
+  }
+
+  return slug; // Return slug as is if no changes are needed
+};
+
+export async function generateMetadata({ params }) {
+  const awaitedParams = await params; // Await the params
+  const { slug, locale } = awaitedParams;
   setRequestLocale(locale);
-  const page = await getPageBySlug("terms-of-use", locale);
+  const slugs = adjustedSlug(locale, "terms-of-use");
+  const page = await getPageBySlug(slugs, locale);
   return {
     title: `${page.title} | Quorixia`,
   };
 }
 
-
-const BlogInner = async ({ params: { locale } }) => {
-  const page = await getPageBySlug("terms-of-use", locale);
+const BlogInner = async ({ params }) => {
+  const awaitedParams = await params; // Await the params
+  const { slug, locale } = awaitedParams;
+  const slugs = adjustedSlug(locale, "terms-of-use");
+  const page = await getPageBySlug(slugs, locale);
   return (
     <section className="policy-inner">
       <div className="_container">
