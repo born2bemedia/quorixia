96ade0499f9674b824364dd63f1466f466262d96
SergWebAcceptance <serg.web@acceptance.ltd>
2024-12-09 13:28:15 +0200

    terms

--- src/middleware.js
+++ src/middleware.js
@@ -1,24 +1,9 @@
 import createMiddleware from 'next-intl/middleware';
-import { localePrefix, locales } from './navigation';
-
-export default createMiddleware({
-  locales,
-  localePrefix,
-  defaultLocale: 'en'
-});
-
-// only applies this middleware to files in the app directory
+import { routing } from './navigation';
+ 
+export default createMiddleware(routing);
+ 
 export const config = {
-  matcher: [
-    // Enable a redirect to a matching locale at the root
-    '/',
-
-    // Set a cookie to remember the previous locale for
-    // all requests that have a locale prefix
-    '/(de|en|it)/:path*',
-
-    // Enable redirects that add missing locales
-    // (e.g. `/pathnames` -> `/en/pathnames`)
-    '/((?!_next|_vercel|.*\\..*).*)'
-  ]
+  // Match only internationalized pathnames
+  matcher: ['/', '/(de|en|it)/:path*']
 };
--- src/navigation.js
+++ src/navigation.js
@@ -1,7 +1,15 @@
-import { createSharedPathnamesNavigation } from 'next-intl/navigation';
-
-export const locales = ['en', 'de', 'it'];
-export const localePrefix = 'as-needed';
-
-export const { Link, redirect, usePathname, useRouter } =
-createSharedPathnamesNavigation({ locales, localePrefix });
+import {defineRouting} from 'next-intl/routing';
+import {createNavigation} from 'next-intl/navigation';
+ 
+export const routing = defineRouting({
+  // A list of all locales that are supported
+  locales: ['en', 'de', 'it'],
+ 
+  // Used when no locale matches
+  defaultLocale: 'en'
+});
+ 
+// Lightweight wrappers around Next.js' navigation APIs
+// that will consider the routing configuration
+export const {Link, redirect, usePathname, useRouter, getPathname} =
+  createNavigation(routing);
